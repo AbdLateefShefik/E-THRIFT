c2b57186ce326a7ea320c75036202b9147009cca
AbdLateef Shefik <lateefshefik295@gmail.com>
2023-02-02 11:22:28 +0100

    shefik

--- Persistence/Database/ApplicationContext.cs
+++ Persistence/Database/ApplicationContext.cs
@@ -1,5 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Persistence.Database;
 
-public class ApplicationContext
+public class ApplicationContext :DbContext
 {
+    public ApplicationContext(DbContextOptions options) : base(options)
+    {
+        
+    }
 }
--- Persistence/Repositories/RepositoryAsync.cs
+++ Persistence/Repositories/RepositoryAsync.cs
@@ -4,20 +4,27 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Application.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Database;
 
 namespace Persistence.Repositories
 {
-          public class RepositoryAsync<T> : IRepository<T> where T : class
+          public class RepositoryAsync<T> :IRepository<T> where T : class, new()
           {
-                    public Task<T> AddAsync(T entity)
+              public ApplicationContext _context;
+              private IRepository<T> _repositoryImplementation;
+              
+                    public async Task<T> AddAsync(T entity)
                     {
-                              throw new NotImplementedException();
+                        _context.Set<T>().AddAsync(entity);
+                        await _context.SaveChangesAsync();
+                        return entity;
                     }
 
-                    public Task<IReadOnlyList<T>> GetAllAsync(Expression<Func<T, bool>> expression)
+                    public async Task<IReadOnlyList<T>> GetAllAsync(Expression<Func<T, bool>> expression)
                     {
-                              throw new NotImplementedException();
-                    }
+                        return await _context.Set<T>().ToListAsync();
+                    } 
 
                     public Task<IReadOnlyList<T>> GetAllByConditionAsync(Expression<Func<T, bool>> expression)
                     {
@@ -25,7 +32,8 @@ namespace Persistence.Repositories
                     }
                     public Task<T> GetAsync(Expression<Func<T, bool>> expression)
                     {
-                              throw new NotImplementedException();
+                        var ans = _context.Set<T>().FirstOrDefaultAsync(expression);
+                        return ans;
                     }
           }
 }
